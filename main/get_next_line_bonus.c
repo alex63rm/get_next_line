5c23ee3d3d88ed911ef0546c6693cc699b5e6764
Alex Rodriguez <alex.rodriguez1003@gmail.com>
2022-11-19 12:54:37 +0100

    paco ok, bonus ok

---
+++ main/get_next_line_bonus.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 22:04:28 by alejarod          #+#    #+#             */
+/*   Updated: 2022/11/19 12:53:16 by alejarod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*ft_read_loop(int fd, char *stash, char *buf)
+{
+	char	*aux;
+	ssize_t	read_size;
+
+	read_size = 1;
+	while (read_size > 0)
+	{
+		read_size = read(fd, buf, BUFFER_SIZE);
+		if (read_size == -1)
+			return (0);
+		if (read_size == 0)
+			break ;
+		if (!stash)
+			stash = ft_strdup("");
+		buf[read_size] = '\0';
+		aux = stash;
+		stash = ft_strjoin(aux, buf);
+		free (aux);
+		aux = NULL;
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+	return (stash);
+}
+
+static char	*ft_next_stash(char *line)
+{
+	size_t	i;
+	char	*stash;
+
+	i = 0;
+	while (line[i] && line[i] != '\n')
+		i++;
+	if (line[i] == '\0')
+		return (0);
+	stash = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (*stash == '\0')
+	{
+		free (stash);
+		stash = NULL;
+	}
+	line[i + 1] = '\0';
+	return (stash);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*stash[1024];
+
+	if (fd < 0 || fd > 1024 || BUFFER_SIZE <= 0)
+		return (0);
+	buf = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buf)
+		return (0);
+	line = ft_read_loop(fd, stash[fd], buf);
+	free (buf);
+	if (!line)
+	{
+		free (stash[fd]);
+		stash[fd] = NULL;
+		return (0);
+	}
+	stash[fd] = ft_next_stash(line);
+	return (line);
+}
---
+++ main/get_next_line_utils_bonus.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/19 12:50:21 by alejarod          #+#    #+#             */
+/*   Updated: 2022/11/19 12:52:57 by alejarod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	size_t	i;
+	char	*dst;
+
+	i = 0;
+	if (!src)
+		return (0);
+	dst = (char *)malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (!dst)
+		return (0);
+	while (src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*ptr;
+
+	i = 0;
+	j = 0;
+	if (!s1 || !s2)
+		return (0);
+	ptr = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!ptr)
+		return (0);
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == (char)c)
+			return (str);
+		i++;
+	}
+	if (c == '\0')
+		return (str);
+	return (NULL);
+}
+
+char	*ft_substr(char *str, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+
+	if (!str)
+		return (0);
+	i = 0;
+	if (start > ft_strlen(str))
+		return (NULL);
+	if (len > ft_strlen(str))
+		len = ft_strlen(str);
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (NULL);
+	while (str[start] && i < len)
+	{
+		ptr[i] = str[start];
+		start++;
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
