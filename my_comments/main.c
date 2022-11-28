2e086f132082c3523a7997c057471abf8bb8d615
Alex Rodriguez <alex.rodriguez1003@gmail.com>
2022-11-28 13:43:12 +0100

    updated main

--- my_comments/main.c
+++ my_comments/main.c
@@ -6,27 +6,32 @@
 /*   By: alejarod <alejarod@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 20:29:22 by alejarod          #+#    #+#             */
-/*   Updated: 2022/11/13 22:05:52 by alejarod         ###   ########.fr       */
+/*   Updated: 2022/11/28 13:42:53 by alejarod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
- int	main(void)
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+ int    main(void)
  {
-	int	fd;
-	char *str;
-	// open the file
-	fd = open("book.txt", O_RDONLY);
-	if (fd == -1)
-		return (-1);
-	else
-	{
-		str = get_next_line(fd);
-		printf("")
-		// close after using
-		close(fd);
-		free(str);
-		return (0);
-	}
+    int fd;
+    char *str;
+    size_t  i;
+    // open the file
+    i = 1;
+    fd = open("book.txt", O_RDONLY);
+        str = get_next_line(fd);
+        while (str)
+        {
+            printf("%zu: %s", i, str);
+            free(str);
+            str = get_next_line(fd);
+            i++;
+        }
+        printf("%zu: %s", i, str);
+        // close after using
+        close(fd);
+        return (0);
  }
